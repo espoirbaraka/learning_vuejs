bb880163cd692844676e9acc0d49fe37c72423f5
Baraka Bigega Espoir <49196039+espoirbaraka@users.noreply.github.com>
2022-04-30 20:37:33 +0200

    What a app-bar?? kiekkk

--- src/views/MyHome.vue
+++ src/views/MyHome.vue
@@ -1,5 +1,21 @@
 <template>
-    <div class="home">
-        Home page
-    </div>
+  <div class="home">
+    <v-list flat>
+
+        <v-list-item>
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox :input-value="active"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>Notifications</v-list-item-title>
+              
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+
+        
+    </v-list>
+  </div>
 </template>
